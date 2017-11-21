a8d76de99480aae157b91b582b33917a60c77a98
ArVaganov <ar.vaganov@gmail.com>
2017-11-21 22:55:28 +0500

    SCSS syntax config and several variables for UI was added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Test.css'
 
 class App extends Component {
   render() {
@@ -8,7 +9,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Добро пожаловать</h2>
+          <h2 className="testclasses">Yo yo here!</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
