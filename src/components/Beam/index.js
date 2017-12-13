e6c4a7243b128fb8126bdf2fc4586044fb0f38ea
ArVaganov <ar.vaganov@gmail.com>
2017-12-13 23:27:33 +0500

    some light difraction was added

--- src/components/Beam/index.js
+++ src/components/Beam/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import './Beam.css'
 
 const Beam = (props) => {
+    let c = props.coordinates
     return (
         <svg style={{ position: 'absolute', left: 0 }}>
-            <line {...props.coordinates} style={{ stroke: props.color }} transform={`translate(${document.body.clientWidth / 2}, 90)`} />
+            <path d={`M ${c.x1} ${c.y1} L ${c.x2} ${c.y2} L ${c.x3} ${c.y3} L ${c.x4} ${c.y4}`} style={{ stroke: props.color }} transform={`translate(${document.body.clientWidth / 2}, 90)`} />
+            <circle cx={c.x2} cy={c.y2} r="3" transform={`translate(${document.body.clientWidth / 2}, 90)`} fill={props.color} />
+            <circle cx={c.x3} cy={c.y3} r="3" transform={`translate(${document.body.clientWidth / 2}, 90)`} fill={props.color} />
         </svg>
     )
 }
