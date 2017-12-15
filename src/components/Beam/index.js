c4e5f60f5740f0ea58a269286662578a3c130e7f
ArVaganov <ar.vaganov@gmail.com>
2017-12-15 02:04:42 -0800

    BEM refractoring

--- src/components/Beam/index.js
+++ src/components/Beam/index.js
@@ -4,10 +4,26 @@ import './Beam.css'
 const Beam = (props) => {
     let c = props.coordinates
     return (
-        <svg style={{ position: 'absolute', left: 0 }}>
-            <path d={`M ${c.x1} ${c.y1} L ${c.x2} ${c.y2} L ${c.x3} ${c.y3} L ${c.x4} ${c.y4}`} style={{ stroke: props.color }} transform={`translate(${document.body.clientWidth / 2}, 90)`} />
-            <circle cx={c.x2} cy={c.y2} r="3" transform={`translate(${document.body.clientWidth / 2}, 90)`} fill={props.color} />
-            <circle cx={c.x3} cy={c.y3} r="3" transform={`translate(${document.body.clientWidth / 2}, 90)`} fill={props.color} />
+        <svg className="beam__scene">
+            <path className="beam__ray"
+                d={`M ${c.x1} ${c.y1} L ${c.x2} ${c.y2} L ${c.x3} ${c.y3} L ${c.x4} ${c.y4}`}
+                style={{ stroke: props.color }}
+                transform={`translate(${document.body.clientWidth / 2}, 90)`}
+            />
+            <circle className="beam__collision-point"
+                cx={c.x2}
+                cy={c.y2}
+                r="3"
+                transform={`translate(${document.body.clientWidth / 2}, 90)`}
+                fill={props.color}
+            />
+            <circle className="beam__collision-point"
+                cx={c.x3}
+                cy={c.y3}
+                r="3"
+                transform={`translate(${document.body.clientWidth / 2}, 90)`}
+                fill={props.color}
+            />
         </svg>
     )
 }
--- src/components/Prism/index.js
+++ src/components/Prism/index.js
@@ -4,15 +4,17 @@ import './Prism.css'
 const Prism = (props) => {
     let { A, B, C } = props.coordinates
     return (
-        <svg>
-            <circle cx="0"
+        <svg className="prism__scene">
+            <circle className="prism__circle"
+                cx="0"
                 cy="0"
                 r={props.radius - 10}
                 transform={`translate(${document.body.clientWidth / 2}, 90)`}
-                fill="transparent"
-                stroke='dimgray'
-                strokeWidth='2' />
-            <polygon points={`${A[0]},${A[1]}, ${B[0]},${B[1]}, ${C[0]},${C[1]}`} transform={`translate(${document.body.clientWidth / 2}, 90)`} />
+            />
+            <polygon className="prism__shape"
+                points={`${A[0]},${A[1]}, ${B[0]},${B[1]}, ${C[0]},${C[1]}`}
+                transform={`translate(${document.body.clientWidth / 2}, 90)`}
+            />
         </svg>
     )
 }
